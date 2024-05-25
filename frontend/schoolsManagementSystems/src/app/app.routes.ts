d674a77839fdfd81b3efa7f58eba0f4445980fb8
Ofori01 <128515949+Ofori01@users.noreply.github.com>
2024-05-25 17:26:11 +0000

    Initialized pages and routing

--- frontend/schoolsManagementSystems/src/app/app.routes.ts
+++ frontend/schoolsManagementSystems/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', 
+    loadChildren: ()=> import('./pages/home/home.module').then(
+        m=>m.HomeModule
+    ),
+    pathMatch: 'full'
+ },
+ {
+    path: 'dashboard',
+    loadChildren: ()=> import('./pages/dashboard/dashboard.module').then(
+        m => m.DashboardModule
+    ),
+    pathMatch: 'full'
+ }
+];
