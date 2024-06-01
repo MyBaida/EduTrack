e0f130f9527e684d231e877b515c843e54c74f5a
Ofori01 <128515949+Ofori01@users.noreply.github.com>
2024-06-01 12:21:04 +0000

    Implemented login service, login interface. Set token in local storage.

--- frontend/school-management-system/src/app/services/users/users.service.ts
+++ frontend/school-management-system/src/app/services/users/users.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
+import { loginResponse } from './interfaces/login';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,30 @@ import { Injectable } from '@angular/core';
 export class UsersService {
 
   constructor(private http:HttpClient) { }
+
+  private loginUrl = 'http://127.0.0.1:8000/api/users/login';
+
+  loginSatus =  new BehaviorSubject<boolean>(false);
+  loginStatus$ = this.loginSatus.asObservable();
+
+
+  Login(username: string, passowrd:string){
+    return this.http.post<loginResponse>(this.loginUrl,{username,passowrd}).pipe(
+      tap(
+        (response)=> {
+          if(response.token){
+            this.loginSatus.next(true);
+            localStorage.setItem('token', response.token);
+          }
+          else{
+            this.loginSatus.next(false)
+          }
+          
+        }
+      )
+    )
+  }
+
+
+
 }
